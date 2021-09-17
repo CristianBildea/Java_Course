1b8f9a7803bdceb2cafc7b751f93d68ed03c8b92
Cristian <c_bildea@live.concordia.ca>
2021-09-16 21:03:46 -0400

    End of class 2

---
+++ src/ca/javacourse/Class_02/VariablePractice1.java
@@ -0,0 +1,45 @@
+package ca.javacourse.Class_02;
+
+public class VariablePractice1 {
+
+    /* This code
+    will not run */
+
+    //This is a line level comment.
+
+    public static void main(String[] args) {
+
+        double gasUsage = 1.1;
+
+        float waterUsage = 2.2f;
+
+
+
+        int counter; //definitian of variable
+        counter = 0; //variable of value
+
+        int counter2 = 0;
+
+        String myName = "Brendan";
+
+        long myValue = (long)4.0;
+
+        //double counter2 =0.0;
+
+
+
+
+        System.out.println("A");   // Prints "A" to the screen.
+
+        boolean expired = true;
+
+        String fullName = "Brendan Wood"; //Ref
+
+        char my_initial = 'B'; // Value
+
+        char choice = 'A'; // for char always use ' and single letter
+
+    }
+
+
+}
